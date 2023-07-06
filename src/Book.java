04ad62bc86ebb4ed9830280fea1c2c4a5ad39add
Maksim <maks.kartashov.93@inbox.ru>
2023-07-06 20:21:37 +0200

    дз12

--- src/Book.java
+++ src/Book.java
@@ -2,13 +2,19 @@ import com.sun.jdi.connect.Connector;
 
 public class Book {
     final String nameBook;
-    final String author;
+    final Author author;
     int est;
 
-    public Book(String nameBook, String author, int est) {
+    public Book(String nameBook, Author author, int est) {
         this.nameBook = nameBook;
         this.author = author;
         this.est = est;
+    }public String getNameBook(){
+        return this.nameBook;
+    }public Author getAuthor(){
+        return this.author;
+    }public int getEst(){
+        return this.est;
     }
 
     public void setEst(int est) {
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
        Author shattam = new Author("Максим","Шаттам");
         Author barto=new Author("Агния","Барто");
-        Book igrirazuma=new Book("Игры разума","Максим Шаттам",2010);
-        Book stihi=new Book("Стихи детские","Агния Барто",2020);
+        Book igrirazuma=new Book("Игры разума",shattam,2010);
+        Book stihi=new Book("Стихи детские",barto,2020);
         System.out.println("Автор :"+shattam.getNameAuthor()+" "+shattam.getSurnameAuthor());
         System.out.println("Название :"+igrirazuma.nameBook);
         System.out.println("Год выпуска "+igrirazuma.est);
